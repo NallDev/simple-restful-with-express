9bf3d23e0a15e19d59ca273e97001fb3fa763cc7
afrinaldiGenzo <afrinaldi190401@gmail.com>
2023-07-16 22:57:22 +0700

    integrated API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
 const port = 3000;
 const app = express();
@@ -18,9 +19,38 @@ db.mongoose.connect(db.url).then((result) => {
 });
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Hellow'
-    });
+    fetch('http://localhost:3000/api/posts/')
+        .then((res) => res.json())
+        .then((json) => {
+            res.render('home.ejs', {
+                data: json
+            })
+        });
+});
+
+app.post('/', (req, res) => {
+    const id = '64b408ff0e1e2f353a2d1c3a'
+    const title = req.body.title
+    const body = req.body.body
+    const reqBody = {
+        title: title,
+        body: body,
+        published: true,
+    }
+    fetch(`http://localhost:3000/api/posts/${id}`, {
+            method: 'put',
+            body: JSON.stringify(reqBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((json) => {
+            res.redirect('/')
+        })
+        .catch((err) => {
+            console.log(err)
+        });
 });
 
 require('./app/routes/post.routes')(app)
